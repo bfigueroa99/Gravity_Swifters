4efd2f54079fac2842b878d8ea22dbaed5f6a898
Lucas-Maldo <89178081+Lucas-Maldo@users.noreply.github.com>
2024-04-14 22:07:20 -0400

    Changed physics for player and fluidity in jumping movement

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,11 +7,17 @@ public class PlayerController : MonoBehaviour
 {
     private Rigidbody2D rb;
     private float horizontalMovement;
-    public float movementSpeed= 2f; 
-    public float jumpForce= 10f;
-    private bool isGrounded= false;
-    private bool isTopGrounded= false;
-    
+    public float movementSpeed = 2f; 
+    public float jumpForce = 50f;
+    public float gravityScale = 2f;
+    public float gravitySense = 1f;
+    public float jumpTimer = 1f;
+    private bool pressedJump = false;
+    private bool releasedJump = false;
+    private bool startTimer = false;
+    private float timer;
+    public bool isGrounded = false;
+    public bool isTopGrounded = false;
     private bool isInverted= false;
     private float jumpTimeTracker;
     public float jumpTime;
@@ -19,6 +25,7 @@ public class PlayerController : MonoBehaviour
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        timer = jumpTimer;
     }
 
     
@@ -27,76 +34,115 @@ public class PlayerController : MonoBehaviour
     {   // Movimiento en eje X y salto
         float horizontalMovement= Input.GetAxis("Horizontal");
         rb.velocity= new Vector2(horizontalMovement * movementSpeed, rb.velocity.y);
-
-        if (Input.GetButtonDown("Jump") && isGrounded)
-        {
-            Debug.Log("Salto activado");
-            rb.velocity= new Vector2(rb.velocity.x, jumpForce);
-            isGrounded= false;
-            isJumping= true;
-            jumpTimeTracker= jumpTime;
+        if (Input.GetButtonDown("Jump") && (isGrounded || isTopGrounded)) {
+                    pressedJump = true;
+                    isGrounded = false;
+                    isTopGrounded = false;
         }
 
-        if (Input.GetButtonDown("Jump") && isTopGrounded)
-        {
-            Debug.Log("Salto activado");
-            rb.velocity= new Vector2(rb.velocity.x, -jumpForce);
-            isTopGrounded= false;
-            isJumping= true;
-            jumpTimeTracker= jumpTime;
+        if (Input.GetButtonUp("Jump")) {
+            releasedJump = true;
         }
 
-        if (Input.GetKey(KeyCode.Space) && isJumping){
-            if (jumpTimeTracker > 0){
-                if (isInverted)
-                {
-                    rb.velocity= new Vector2(rb.velocity.x, -jumpForce);
-                    
-                }
-                else {
-                    rb.velocity= new Vector2(rb.velocity.x, jumpForce);
-                }
-                jumpTimeTracker -= Time.deltaTime;
-            } else {
-                isJumping= false;
+        if (startTimer) {
+            timer -= Time.deltaTime;
+            if (timer <= 0) {
+                releasedJump = true;
             }
-            
         }
+        //     if (Input.GetButtonDown("Jump") && isGrounded)
+        //     {
+        //         Debug.Log("Salto activado");
+        //         rb.velocity= new Vector2(rb.velocity.x, jumpForce);
+        //         isGrounded= false;
+        //         isJumping= true;
+        //         jumpTimeTracker= jumpTime;
+        //     }
 
-        if (Input.GetKeyDown(KeyCode.LeftShift))
+        //     if (Input.GetButtonDown("Jump") && isTopGrounded)
+        //     {
+        //         Debug.Log("Salto activado");
+        //         rb.velocity= new Vector2(rb.velocity.x, -jumpForce);
+        //         isTopGrounded= false;
+        //         isJumping= true;
+        //         jumpTimeTracker= jumpTime;
+        //     }
+        
+        // if (Input.GetKey(KeyCode.Space) && isJumping){
+        //     if (jumpTimeTracker > 0){
+        //         if (isInverted)
+        //         {
+        //             rb.velocity= new Vector2(rb.velocity.x, -jumpForce);
+                    
+        //         }
+        //         else {
+        //             rb.velocity= new Vector2(rb.velocity.x, jumpForce);
+        //         }
+        //         jumpTimeTracker -= Time.deltaTime;
+        //     } else {
+        //         isJumping= false;
+        //     }
+            
+        // }
+
+        if (Input.GetKeyDown(KeyCode.LeftShift) && (isGrounded || isTopGrounded))
         {
             Debug.Log("Cambio de gravedad");
             isInverted= !isInverted;
+            gravitySense *= -1;
             rb.gravityScale *= -1;
+            isGrounded = false;
+            isTopGrounded = false;
             CharacterRotation();
         }
     }
+        private void FixedUpdate() {
+        if (pressedJump) {
+            StartJump();
+        }
+
+        if (releasedJump) {
+            StopJump();
+        }
+    }
+        private void StartJump() {
+        rb.gravityScale = 0;
+        rb.AddForce(new Vector2(0, gravitySense * jumpForce), ForceMode2D.Impulse);
+        pressedJump = false;
+        startTimer = true;
+    }
+
+    private void StopJump() {
+        rb.gravityScale = gravityScale * gravitySense;
+        releasedJump = false;
+        timer = jumpTimer;
+        startTimer = false;
+    }
 
     void CharacterRotation(){
-        if (isTopGrounded == false)
+        if (gravitySense == -1)
         {
             transform.eulerAngles= new Vector3(0, 0, 180);
         }
-        else 
+        if (gravitySense == 1)
         {
             transform.eulerAngles= new Vector3(0, 0, 0);
         }
-
-        isTopGrounded= !isTopGrounded;
-    
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.tag == "Ground")
         {   
-            Debug.Log("Tocando suelo");
             isGrounded= true;
+        } else {
+            isGrounded= false;
         }
         if (collision.gameObject.tag == "TopGround")
         {
-            Debug.Log("Tocando techo");
             isTopGrounded= true;
+        }else {
+            isTopGrounded= false;
         }
     }
 }
