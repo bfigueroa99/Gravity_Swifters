baf4984c13c4cc4387dc3cae69e03bcdfe9f006f
mjanas1 <mjanas@miuandes.cl>
2024-04-20 21:52:35 -0400

    ItemPickup Script

---
+++ Assets/Scripts/ItemPickup.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemPickup : MonoBehaviour
+{
+    public GameObject player;
+    private bool pickupAllowed;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (pickupAllowed && Input.GetKeyDown(KeyCode.E))
+        {
+            PickUp();
+        }
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag.Equals("Player"))
+        {
+            pickupAllowed = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag.Equals("Player"))
+        {
+            pickupAllowed = false;
+        }
+    }
+
+    private void PickUp()
+    {
+        player.GetComponent<PlayerPowers>().AddItem(gameObject);
+        Destroy(gameObject);
+    }
+}
