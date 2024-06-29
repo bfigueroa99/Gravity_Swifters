c13e4c9349690b806973a0193b9883fd1301753f
VincenzoBraghetto <vabraghetto@miuandes.cl>
2024-06-29 17:29:27 -0400

    fix boss sound after dying

--- Assets/Scripts/LimitCaptor.cs
+++ Assets/Scripts/LimitCaptor.cs
@@ -15,12 +15,18 @@ public class PlatformTrigger : MonoBehaviour
         platformCollider = GetComponent<BoxCollider2D>();
     }
 
+    private void Update()
+    {
+        
+    }
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Player"))
         {  
             Debug.Log("Player touched the platform.");
             triggeredBossFight = true;
+            StartCoroutine(ResetTriggeredBossFight());
             StartCoroutine(DisableIsTriggerAfterDelay(1f)); 
         }
     }
@@ -33,5 +39,11 @@ public class PlatformTrigger : MonoBehaviour
         platformCollider.isTrigger = false;
         otherPlatformCollider.isTrigger = false;
         Debug.Log("Platform trigger option disabled.");
+    }
+
+        IEnumerator ResetTriggeredBossFight()
+    {
+        yield return new WaitForSeconds(3f); 
+        triggeredBossFight = false;
     }
 }
