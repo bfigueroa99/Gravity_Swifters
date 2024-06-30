390530bd0255c9dc8e274ae300c07efd7cc00dd6
Victoria Guerriero <vaguerriero@miuandes.cl>
2024-06-29 22:06:42 -0400

    added things for history

--- Assets/Scripts/UIController.cs
+++ Assets/Scripts/UIController.cs
@@ -1,19 +1,29 @@
 using UnityEngine;
 using UnityEngine.UI;
 using System.Collections;
+using System.Collections.Generic;
 
 public class UIController : MonoBehaviour
 {
-    public Image displayImage;
+    public List<Image> displayImages;
+    private int currentImageIndex = 0;
     private bool canHide;
     public static bool pickedUpRelic;
     public static bool closedRelicText;
 
-    public void ShowImage()
-    {   
-        if (displayImage != null)
+    void Start()
+    {
+        foreach (Image img in displayImages)
         {
-            displayImage.gameObject.SetActive(true);
+            img.gameObject.SetActive(false);
+        }
+    }
+
+    public void ShowImage()
+    {
+        if (displayImages.Count > 0 && displayImages[currentImageIndex] != null)
+        {
+            displayImages[currentImageIndex].gameObject.SetActive(true);
             pickedUpRelic = true;
 
             Debug.Log("Image displayed");
@@ -21,14 +31,14 @@ public class UIController : MonoBehaviour
         }
         else
         {
-            Debug.LogWarning("Display Image is not assigned!");
+            Debug.LogWarning("Display Images are not assigned or the list is empty!");
         }
     }
 
     private IEnumerator HideImageAfterDelay()
     {
         yield return new WaitForSeconds(0.5f);
-        if (displayImage != null)
+        if (displayImages.Count > 0 && displayImages[currentImageIndex] != null)
         {
             canHide = true;
             Debug.Log("Image can be hidden");
@@ -36,18 +46,32 @@ public class UIController : MonoBehaviour
     }
 
     void Update()
-    {    
+    {
         pickedUpRelic = false;
         closedRelicText = false;
 
-        if (displayImage != null && canHide == true && Input.GetKeyDown(KeyCode.E))
+        if (displayImages.Count > 0 && canHide)
         {
-            closedRelicText = true;
+            if (Input.GetKeyDown(KeyCode.Return))
+            {
+                displayImages[currentImageIndex].gameObject.SetActive(false);
+                Debug.Log("Image hidden by pressing Enter");
 
-            displayImage.gameObject.SetActive(false);
-            Debug.Log("Image hidden by pressing E");
+                currentImageIndex = (currentImageIndex + 1) % displayImages.Count;
+                displayImages[currentImageIndex].gameObject.SetActive(true);
+                Debug.Log("Next image displayed");
 
-            canHide = false;
+                canHide = false;
+                StartCoroutine(HideImageAfterDelay());
+            }
+            else if (Input.GetKeyDown(KeyCode.E))
+            {
+                displayImages[currentImageIndex].gameObject.SetActive(false);
+                Debug.Log("Image hidden by pressing E");
+                closedRelicText = true;
+
+                canHide = false;
+            }
         }
     }
 }
